26dcecf19cacc226768c5961f79ebe27109c53ae
Siddhesh More <msiddhesh008@gmail.com>
2023-04-22 15:29:27 +0530

    third dep

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -20,10 +20,11 @@ const SignIn = () => {
       <div class="bottom" aria-hidden="true">FireChat</div>      
       </section>
 
-      <div className="col-12 d-flex justify-content-center flex-column align-items-center">
+      <div className="col-12 d-flex justify-content-center flex-column gap-5 align-items-center">
       <button 
       onClick={signInWithGoogle}
       className='button-42 p-3 fs-4 fw-bold'>Login with <i class="bi bi-google fw-normal">oogle</i></button>
+      <p className='name fw-normal'>Made with love by <span style={{color:"#007aff"}}>S!ddhesh</span>.</p>
       </div>
 
       </div>
